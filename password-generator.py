1f6cac8dc0b748d422737125c8c768b319d7748e
Stephen Kraemer <straemer@gmail.com>
2014-04-12 19:06:46 -0400

    use /dev/random for better passwords than pseudo-random ones from python random

--- password-generator.py
+++ password-generator.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python3
 
 import sys
-import random
+import os
+import struct
+
+#taken from http://stackoverflow.com/a/14720865
+_random_source = open("/dev/random", "rb")
+
+def random_bytes(len):
+    return _random_source.read(len)
+
+def unpack_uint32(bytes):
+    tup = struct.unpack("I", bytes)
+    return tup[0]
+
+UINT32_MAX = 0xffffffff
+def randint(low, high):
+    """
+    Return a random integer in the range [low, high], including
+    both endpoints.
+    """
+    n = (high - low) + 1
+    assert n >= 1
+    scale_factor = n / float(UINT32_MAX + 1)
+    random_uint32 = unpack_uint32(random_bytes(4))
+    result = int(scale_factor * random_uint32) + low
+    return result
 
 allChars = '`1234567890-=qwertyuiop[]\\asdfghjkl;\'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>?'
 
@@ -10,6 +34,6 @@ length = int(sys.argv[1])
 password = ''
 
 for i in range(length):
-    password += random.choice(allChars)
+    password += allChars[randint(0,len(allChars))]
 
 print(password)
